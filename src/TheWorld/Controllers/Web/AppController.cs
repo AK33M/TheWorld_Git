422fffe2920bbd1ab6582e2f96a84dbcd4576a1c
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2016-04-07 12:11:32 +0100

    MVC 6 module

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using TheWorld.Services;
+using TheWorld.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+
+        public AppController(IMailService service)
+        {
+            _mailService = service;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var email = Startup.Configuration["AppSettings:SiteEmailAddress"];
+
+                if (string.IsNullOrWhiteSpace(email))
+                {
+                    ModelState.AddModelError("", "Could not send email, configuration problem");
+                }
+
+                if(_mailService.SendMail(email,
+                    email,
+                    $"Contact Page from ({model.Name})",
+                    model.Message))
+                {
+                    ModelState.Clear();
+
+                    ViewBag.Message = "Mail Sent. Thanks!";
+                }
+            }
+
+            return View();
+        }
+    }
+}
