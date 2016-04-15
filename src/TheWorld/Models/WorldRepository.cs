f8bab566f23c6bf8c8281e2de1eb6c696cd054c8
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2016-04-15 17:18:34 +0100

    end of Ef7 (with logging)

--- src/TheWorld/Models/WorldRepository.cs
+++ src/TheWorld/Models/WorldRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.Entity;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,24 +10,42 @@ namespace TheWorld.Models
     public class WorldRepository : IWorldRepository
     {
         private WorldContext _context;
+        private ILogger<WorldRepository> _logger;
 
-        public WorldRepository(WorldContext context)
+        public WorldRepository(WorldContext context, ILogger<WorldRepository> logger)
         {
+            _logger = logger;
             _context = context;
         }
         public IEnumerable<Trip> GetAllTrips()
         {
-            return _context.Trips
-                .OrderBy(t => t.Name)
-                .ToList();
+            try
+            {
+                return _context.Trips
+                    .OrderBy(t => t.Name)
+                    .ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Could not get trips from databse", ex);
+                return null;
+            }
         }
 
         public IEnumerable<Trip> GetAllTripsWithStops()
         {
-            return _context.Trips
-                .Include(x => x.Stops)
-                .OrderBy(t => t.Name)
-                .ToList();
+            try
+            {
+                return _context.Trips
+                    .Include(x => x.Stops)
+                    .OrderBy(t => t.Name)
+                    .ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Could not get trips with stops from databse", ex);
+                return null;
+            }
         }
     }
 }
